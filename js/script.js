c7daface69861b4f9ca040668b3d9e9f1a919b20
amraldroubie <amraldroubie@hotmail.com>
2023-04-11 15:22:18 +0300

    video load

--- js/script.js
+++ js/script.js
@@ -129,13 +129,9 @@ if (document.querySelector(".project-slider")) {
 
 if (document.querySelector(".landing-header video")) {
   const headerVideo = document.querySelector(".landing-header video");
-  window.onload = function () {
-    headerVideo.onloaddata = function () {
-      headerVideo.querySelector("source").src = headerVideo
-        .querySelector("source")
-        .getAttribute("data-src");
-      document.querySelector(".landing-header").style.backgroundImage = "none";
-    };
+  headerVideo.onloadeddata = function () {
+    this.classList.remove("d-none");
+    document.querySelector(".landing-header").style.backgroundImage = "none";
   };
 }
 
